877730b79f7824dea1eab7fe1ef8dcd62f343878
Ulubayam <gizemulubayam@gmail.com>
2019-11-23 00:15:07 +0300

    added unsplash api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import StackGrid from "react-stack-grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+
+    }
+  }
+
+  getData(topic) {
+    axios.get(
+      `https://api.unsplash.com/search/photos?query=${topic}&per_page=20`,
+      {
+        headers: {
+          Authorization:
+            "Client-ID 7e5804804a9e90fddf3cbd411e45c50737a06319ba8204f91f04ce927e2bca87",
+          "Accept-Version": "v1"
+        }
+      }
+    ).then(res => {
+      const images = res.data.results
+      this.setState({ images })
+
+    })
+
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div id="App">
+        <div className="container">
+          <div className="button-wrapper"> 
+          <button className="btn" onClick={this.getData('Autumn')}>Autumn</button>
+          <button className="btn" onClick={this.getData('Cliff')}>Cliff</button>
+          <button className="btn" onClick={this.getData('Ocean')}>Ocean</button>
+        </div>
+        <div className="container">
+          {this.state.images.map((image,i)=> {
+            return (
+              <StackGrid>
+                <img src={image.urls.small}></img>
+              </StackGrid>
+             
+            )
+          })}
+        </div>
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
